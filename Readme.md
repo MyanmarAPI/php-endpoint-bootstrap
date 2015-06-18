1e85eb06d073fec0d3d3b30b083e21e125f38070
Mr-Zer0 <bulletson.geek@gmail.com>
2015-06-18 17:17:16 +0630

    Add formatted abstract model for models.

--- Readme.md
+++ Readme.md
@@ -2,15 +2,108 @@
 ==========================
 This repo is intend to use as boilerplate for various endpoints developed in PHP.
 
-#### Installation
+### Installation
 
 - Clone this repo
 - Change your own remote
 - run composer install
 - place your codes
 
-#### Technology
+##### Applicaiont Environment
 
-- Lumne <Micro Framework from Larave>
-- Fractal <Composer package for REST API>
-- Monog-lite <Composer package for mongodb>
+Create a file with name '.env' in your project root directory. And past the 
+following code in it.
+
+	APP_ENV=local
+	APP_DEBUG=true
+	APP_KEY=Your APP Key
+
+	APP_LOCALE=en
+	APP_FALLBACK_LOCALE=en
+
+	CACHE_DRIVER=memcached
+	SESSION_DRIVER=memcached
+	QUEUE_DRIVER=database
+
+The value for **APP_KEY** should have 32 words which is combined with character 
+and numeric.
+
+##### Config
+
+You can set key value pair for your application config in config/app.php. 
+You shold change the value of **api_key** in it.
+
+### Development
+
+##### Model
+
+Model files should be in app/Model directory. For more query function see 
+[mongo lite documentation](https://github.com/hexcores/mongo-lite)
+
+```php
+
+	<?php namespace App\Model;
+	
+	use App\Model\AbstractModel;
+	
+	class User extends AbstractModel
+	{
+		public function getCollectionName()
+		{
+			return 'users';
+		}
+		
+		public function create(array $data)
+		{
+			// do your stuff here
+		}
+	}
+
+```
+
+```php
+
+	// app/http/routes.php
+	$app->get('/get-users', function ()
+	{
+		dump((new \App\Model\User())->all());
+	});
+
+```
+
+##### Route
+
+All endpoints will be called from elecapi api router only. So your route should not be public. The follwoing example will be the right way.
+
+```php
+
+	// app/http/route.php
+	
+	$app->group(['middleware' => 'auth'], function () use ($app)
+	{
+    	
+    	$app->get('/get-users', function ()
+    	{
+    		dump((new \App\Model\User())->all());
+    	});
+    	
+        // all the rest of your route should be in this scope.
+
+	});
+
+```
+
+***See [lumen documenation](http://lumen.laravel.com/docs) for further more 
+documentation***
+
+### Rules
+
+##### Development
+
+You must follow [psr](http://www.php-fig.org/) standard to develop endpoints.
+
+### Technology
+
+- [Lumne](http://lumen.laravel.com/) <Micro Framework from Larave>
+- [Fractal](http://fractal.thephpleague.com/) <Composer package for REST API>
+- [Monog lite](https://github.com/hexcores/mongo-lite) <Composer package for mongodb>
---
+++ app/Model/AbstractModel.php
@@ -0,0 +1,114 @@
+<?php namespace App\Model;
+
+use Hexcores\MongoLite\Query;
+
+/**
+ * This file is part of elecapi project
+ * Abstract class for application model
+ *
+ * @author  Hexcores Web and Mobile Studio <support@hexcores.com>
+ * @package App\Model
+ */
+abstract class AbstractModel
+{
+    /**
+     * Mongo lite query instance
+     * @var Hexcores\MongoLite\Query
+     */
+    protected $collection;
+
+    /**
+     * Mongodb connection
+     * @var Hexcores\MongoLite\Connection
+     */
+    protected $connection;
+
+    /**
+     * Constructor method for abstract model class
+     */
+    public function __construct()
+    {
+        $this->connection = app('connection');
+
+        $this->collection = new Query($this->connection->collection($this->getCollectionName()));
+    }
+
+    public function getCollection()
+    {
+        return $this->collection;
+    }
+
+    /**
+     * Get all documents from specific collection
+     * @return array
+     */
+    public function all()
+    {
+        return $this->getCollection()->all();
+    }
+
+    /**
+     * Get many document form specific collection
+     * @param  integer $limit  
+     * @param  integer $offset Offset or skip
+     * @return array
+     */
+    public function getMany($limit = 10, $offset = 0)
+    {
+        return $this->getCollection()->all($limit, $offset);
+    }
+
+    /**
+     * Get many document with where statement
+     * @param  string $key   Document key
+     * @param  mix    $value
+     * @param  array  $field
+     * @return array
+     */
+    public function getManyBy($key, $value, array $field = [])
+    {
+        return $this->getCollection()->where($key, '=', $value)->get($field);
+    }
+
+    /**
+     * Get one document by specific key value pair
+     * @param  string $key   Mongo document key
+     * @param  mix    $value
+     * @return Hexcores\MongoLite\Document|null
+     */
+    public function getBy($key, $value)
+    {
+        return $this->getCollection()->first([$key, $value]);
+    }
+
+    /**
+     * Get one document by mongo id
+     * @param  string $id
+     * @return Hexcores\MongoLite\Document|null
+     */
+    public function get($id)
+    {
+        return $this->getCollection()->first($id);
+    }
+
+    /**
+     * Return mongo collection name to be connected
+     *
+     * <code>
+     *     public function getCollectionName()
+     *     {
+     *         return 'user';
+     *     }
+     * </code>
+     * 
+     * @return string
+     */
+    abstract public function getCollectionName();
+
+    /**
+     * Insert newly data to specific mongo collection
+     * @param  array  $data
+     * @return mix
+     */
+    abstract public function create(array $data);
+}
